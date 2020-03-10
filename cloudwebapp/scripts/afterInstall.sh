92f1d675ce5a503033f361ac5056d1ab9e05bfdd
Sarthak Goel <goel.sar@husky.neu.edu>
2020-03-10 18:17:45 -0400

    Adding changes to afterinstall.sh

--- cloudwebapp/scripts/afterInstall.sh
+++ cloudwebapp/scripts/afterInstall.sh
@@ -6,10 +6,7 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/webapp/*
 sudo chmod +x cloudwebapp-0.0.1-SNAPSHOT.jar
 
 #Killing the application
-kill -9 $(ps -ef|grep demo-0.0.1 | grep -v grep | awk '{print $2}')
-
-# #Removing log files
-# sudo rm -rf logs/*.log
+kill -9 $(ps -ef|grep cloudwebapp-0.0.1 | grep -v grep | awk '{print $2}')
 
 source /etc/profile.d/envvariable.sh
-nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/log.txt 2> /home/ubuntu/log.txt < /home/ubuntu/log.txt &
