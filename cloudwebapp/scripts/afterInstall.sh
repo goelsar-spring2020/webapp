a349a4a07fed76e30f880db48e0e28e6193d5627
Sarthak Goel <goel.sar@husky.neu.edu>
2020-03-10 14:58:26 -0400

    Adding all the changes in the afterInstall.sh

--- cloudwebapp/scripts/afterInstall.sh
+++ cloudwebapp/scripts/afterInstall.sh
@@ -3,7 +3,7 @@
 echo "Entered after install hook"
 cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/webapp/*
-sudo chmod +x demo-0.0.1-SNAPSHOT.jar
+sudo chmod +x cloudwebapp-0.0.1-SNAPSHOT.jar
 
 #Killing the application
 kill -9 $(ps -ef|grep demo-0.0.1 | grep -v grep | awk '{print $2}')
@@ -12,4 +12,4 @@ kill -9 $(ps -ef|grep demo-0.0.1 | grep -v grep | awk '{print $2}')
 # sudo rm -rf logs/*.log
 
 source /etc/profile.d/envvariable.sh
-nohup java -jar demo-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
