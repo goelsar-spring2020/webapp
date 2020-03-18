d54bb8f33707ead4b4bf3d41e9965ad9d6c85729
Sarthak Goel <goel.sar@husky.neu.edu>
2020-03-18 05:56:02 -0400

    Adding log4j2 changes

--- cloudwebapp/scripts/afterInstall.sh
+++ cloudwebapp/scripts/afterInstall.sh
@@ -5,14 +5,11 @@ cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/webapp/*
 sudo chmod +x cloudwebapp-0.0.1-SNAPSHOT.jar
 
+source /etc/profile.d/envvariable.sh
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/amazon-cloudwatch-agent-schema.json -s
 #Killing the application
 kill -9 $(ps -ef|grep cloudwebapp-0.0.1 | grep -v grep | awk '{print $2}')
-
 #Removing log files
 sudo rm -rf logs/*.log
-
-source /etc/profile.d/envvariable.sh
 #Running cloudwebapp jar
 nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/log.txt 2> /home/ubuntu/log.txt < /home/ubuntu/log.txt &
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/amazon-cloudwatch-agent-schema.json -s
