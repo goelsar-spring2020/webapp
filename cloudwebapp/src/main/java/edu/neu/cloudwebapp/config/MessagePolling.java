1a47a58f138d0f2c6781ffa29314890dc9bcee17
Sarthak Goel <goel.sar@husky.neu.edu>
2020-04-02 03:26:16 -0400

    Rectifying the code to fullfil the requirment 1

--- cloudwebapp/src/main/java/edu/neu/cloudwebapp/config/MessagePolling.java
+++ cloudwebapp/src/main/java/edu/neu/cloudwebapp/config/MessagePolling.java
@@ -2,6 +2,7 @@ package edu.neu.cloudwebapp.config;
 
 import edu.neu.cloudwebapp.utility.AWSSNS;
 import edu.neu.cloudwebapp.utility.AWSSQS;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -20,7 +21,7 @@ public class MessagePolling {
     private AWSSQS awssqs;
 
     @Scheduled(fixedRate = 10000)
-    public void pollMessage() {
+    public void pollMessage() throws JSONException {
         awssqs.receiveSQSMessage();
     }
 }
